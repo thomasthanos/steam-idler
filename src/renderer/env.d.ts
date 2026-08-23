8b7ff12c8627d503d70c2ee708d842aaf40689b4
Thomas <116557800+thomasthanos@users.noreply.github.com>
2026-08-23 18:56:48 +0300

    Remove source files from assets branch

--- src/renderer/env.d.ts
+++
@@ -1,9 +0,0 @@
-import { SteamAPI, WindowAPI } from '../preload/index'
-
-declare global {
-  const __APP_VERSION__: string
-  interface Window {
-    steam: SteamAPI
-    windowAPI: WindowAPI
-  }
-}
